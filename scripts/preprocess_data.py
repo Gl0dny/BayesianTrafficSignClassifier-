32a02ce1442de3a09568929bb27f80f103d02580
Gl0dny <krystian.glodek1717@gmail.com>
2024-06-10 13:38:01 +0200

    saving numpy files for processed data

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -36,13 +36,16 @@ if __name__ == '__main__':
     images, labels = load_preprocess_gtsrb_data(data_dir)
     print(f'Loaded {len(images)} images with {len(labels)} labels.')
 
-    # # Podział na zestaw treningowy i testowy
-    # X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
-    # print(f'Train set size: {X_train.shape[0]}, Test set size: {X_test.shape[0]}')
+    # Checking data shape
+    print(images.shape, labels.shape)
 
-    # # Zapisywanie przetworzonych danych (opcjonalnie)
-    # np.save('data/X_train.npy', X_train)
-    # np.save('data/X_test.npy', X_test)
-    # np.save('data/y_train.npy', y_train)
-    # np.save('data/y_test.npy', y_test)
+    # Podział na zestaw treningowy i testowy
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
+    print(f'Train set size: {X_train.shape[0]}, Test set size: {X_test.shape[0]}')
+
+    # Zapisywanie przetworzonych danych (opcjonalnie)
+    np.save('data/GTSRB/Traffic_Signs/X_train.npy', X_train)
+    np.save('data/GTSRB/Traffic_Signs/X_test.npy', X_test)
+    np.save('data/GTSRB/Traffic_Signs/y_train.npy', y_train)
+    np.save('data/GTSRB/Traffic_Signs/y_test.npy', y_test)
 
