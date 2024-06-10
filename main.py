9c0a589a93cd331a6ae58c0ed924ae94d12df90c
Gl0dny <krystian.glodek1717@gmail.com>
2024-06-10 11:25:27 +0200

    unpacking gtsrb data

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import os
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+
+python_executable = sys.executable  # dynamicznie pobiera ścieżkę do interpretera Pythona
 
 def main():
-    # Krok 1: Pobranie i rozpakowanie danych
-    print("Step 1: Downloading and extracting GTSRB data...")
-    os.system('python data/download_and_extract_gtsrb.py')
+    # Krok 1: Rozpakowanie danych
+    print("Step 1: Extracting GTSRB data...")
+    subprocess.run([python_executable, 'data/GTSRB/extract_gtsrb.py'], check=True)
 
     # Krok 2: Przetwarzanie danych
     print("Step 2: Preprocessing data...")
-    os.system('python scripts/preprocess_data.py')
+    subprocess.run([python_executable, 'scripts/preprocess_data.py'], check=True)
 
     # Krok 3: Wizualizacja przykładowych danych
     print("Step 3: Visualizing sample data...")
-    os.system('python scripts/visualize_samples.py')
+    subprocess.run([python_executable, 'scripts/visualize_samples.py'], check=True)
 
 if __name__ == '__main__':
     main()
