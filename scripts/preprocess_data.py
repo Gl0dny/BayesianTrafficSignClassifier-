e3176f7fce4adc720c6fcecc1ae693c4989a9557
Gl0dny <krystian.glodek1717@gmail.com>
2024-06-10 14:56:24 +0200

    Hu moments extraction added

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,11 +1,12 @@
 import os
-import cv2
-from PIL import Image
 import numpy as np
+from PIL import Image
 from sklearn.model_selection import train_test_split
+import cv2
 
 def load_preprocess_gtsrb_data(data_dir):
     images = []
+    hu_moments = []
     labels = []
     classes = 43
 
@@ -15,52 +16,67 @@ def load_preprocess_gtsrb_data(data_dir):
             continue
 
         for img_name in os.listdir(class_dir):
-            try:   
-                image = Image.open(class_dir + '/'+ img_name)
+            try:
+                image = Image.open(os.path.join(class_dir, img_name))
                 # Convert the image to grayscale
                 image = image.convert('L')
-                image = image.resize((32,32))
+                image = image.resize((32, 32))
                 image_array = np.array(image)
+
+                # Compute Hu Moments
+                moments = cv2.moments(image_array)
+                hu_moments_image = cv2.HuMoments(moments).flatten()
+
                 images.append(image_array)
+                hu_moments.append(hu_moments_image)
                 labels.append(class_id)
             except Exception as e:
                 print("Error processing image:", e)
 
-    return np.array(images), np.array(labels)
+    return np.array(images), np.array(hu_moments), np.array(labels)
 
 def split_train_test_data(data, test_size=0.2, random_state=42):
     if os.path.exists(os.path.join(data, 'X_train.npy')):
         print("Dane zostały już przetworzone. Ładowanie z plików numpy...")
         X_train = np.load(os.path.join(data, 'X_train.npy'))
         X_test = np.load(os.path.join(data, 'X_test.npy'))
+        hu_train = np.load(os.path.join(data, 'hu_train.npy'))
+        hu_test = np.load(os.path.join(data, 'hu_test.npy'))
         y_train = np.load(os.path.join(data, 'y_train.npy'))
         y_test = np.load(os.path.join(data, 'y_test.npy'))
     else:
         # Wczytywanie i przetwarzanie danych
-        images, labels = load_preprocess_gtsrb_data(data)
+        images, hu_moments, labels = load_preprocess_gtsrb_data(data)
         print(f'Loaded {len(images)} images with {len(labels)} labels.')
 
         # Checking data shape
         print(images.shape, labels.shape)
+        print(hu_moments.shape)
 
         # Podział na zestaw treningowy i testowy
-        X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=test_size, random_state=random_state)
+        X_train, X_test, hu_train, hu_test, y_train, y_test = train_test_split(
+            images, hu_moments, labels, test_size=test_size, random_state=random_state
+        )
         print(f'Train set size: {X_train.shape[0]}, Test set size: {X_test.shape[0]}')
 
-        # Zapisywanie przetworzonych danych (opcjonalnie)
+        # Zapisywanie przetworzonych danych
         np.save(os.path.join(data, 'X_train.npy'), X_train)
         np.save(os.path.join(data, 'X_test.npy'), X_test)
+        np.save(os.path.join(data, 'hu_train.npy'), hu_train)
+        np.save(os.path.join(data, 'hu_test.npy'), hu_test)
         np.save(os.path.join(data, 'y_train.npy'), y_train)
         np.save(os.path.join(data, 'y_test.npy'), y_test)
 
-    return X_train, X_test, y_train, y_test
+    return X_train, X_test, hu_train, hu_test, y_train, y_test
 
 if __name__ == '__main__':
     # Ścieżka do katalogu z danymi
     data_dir = 'data/GTSRB/Traffic_Signs/'
 
     # Podział na zestaw treningowy i testowy
-    X_train, X_test, y_train, y_test = split_train_test_data(data=data_dir,test_size=0.2, random_state=42)
+    X_train, X_test, hu_train, hu_test, y_train, y_test = split_train_test_data(
+        data=data_dir, test_size=0.2, random_state=42
+    )
 
     print(f'Train set size: {X_train.shape[0]}, Test set size: {X_test.shape[0]}')
-
+    print(f'Train Hu moments size: {hu_train.shape[0]}, Test Hu moments size: {hu_test.shape[0]}')
--- scripts/visualize_samples.py
+++ scripts/visualize_samples.py
@@ -1,20 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def show_sample_images(images, labels, num_samples=10):
-    plt.figure(figsize=(10, 2))
+def show_sample_images(images, labels, hu_moments, num_samples=10):
+    plt.figure(figsize=(10, 4))  # Zwiększ wysokość figury, aby zmieścić momenty Hu
     for i in range(num_samples):
-        plt.subplot(1, num_samples, i + 1)
-        # plt.imshow(images[i].reshape(32, 32), cmap='gray')
-        plt.imshow(images[i])
+        plt.subplot(2, num_samples, i + 1)  # Pierwszy rząd: obrazy
+        plt.imshow(images[i], cmap='gray')
         plt.title(f'Label: {labels[i]}')
         plt.axis('off')
+        
+        plt.subplot(2, num_samples, num_samples + i + 1)  # Drugi rząd: momenty Hu
+        hu_text = '\n'.join([f'{moment:.2e}' for moment in hu_moments[i]])  # Formatowanie momentów Hu
+        plt.text(0.5, 0.5, hu_text, ha='center', va='center', wrap=True)
+        plt.axis('off')
+        
     plt.show()
 
 if __name__ == '__main__':
     # Wczytywanie przetworzonych danych
     X_train = np.load('data/GTSRB/Traffic_Signs/X_train.npy')
     y_train = np.load('data/GTSRB/Traffic_Signs/y_train.npy')
+    hu_train = np.load('data/GTSRB/Traffic_Signs/hu_train.npy')
 
-    # Wyświetlanie przykładowych obrazów z zestawu treningowego
-    show_sample_images(X_train, y_train)
+    # Wyświetlanie przykładowych obrazów z zestawu treningowego wraz z momentami Hu
+    show_sample_images(X_train, y_train, hu_train)
