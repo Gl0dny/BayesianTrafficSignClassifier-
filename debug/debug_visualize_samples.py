c7a0ecef86c0c8fbc7fa98b04d6849f3570a8c09
Gl0dny <krystian.glodek1717@gmail.com>
2024-06-11 20:44:04 +0200

    fixed & commented visualize script

--- debug/debug_visualize_samples.py
+++ debug/debug_visualize_samples.py
@@ -8,26 +8,29 @@ def show_sample_images(images, labels, hu_moments, num_samples=10):
     Wyświetla próbki obrazów z odpowiadającymi im momentami Hu.
 
     Parameters:
-    - images: Tablica z obrazami.
-    - labels: Tablica z etykietami klas.
-    - hu_moments: Tablica z momentami Hu.
-    - num_samples: Liczba próbek do wyświetlenia (domyślnie 10).
+    - images (ndarray): Tablica z obrazami.
+    - labels (ndarray): Tablica z etykietami klas.
+    - hu_moments (ndarray): Tablica z momentami Hu.
+    - num_samples (int): Liczba próbek do wyświetlenia (domyślnie 10).
     """
-    plt.figure(figsize=(10, 4))  # Zwiększ wysokość figury, aby zmieścić momenty Hu
+    plt.figure(figsize=(15, 6))  # Ustawienie rozmiaru figury, aby zmieścić więcej informacji
     for i in range(num_samples):
         plt.subplot(2, num_samples, i + 1)  # Pierwszy rząd: obrazy
-        plt.imshow(images[i], cmap='gray')
-        plt.title(f'Label: {labels[i]}')
-        plt.axis('off')
+        plt.imshow(images[i], cmap='gray')  # Wyświetlenie obrazu w skali szarości
+        plt.title(f'Label: {labels[i]}')  # Dodanie tytułu z etykietą klasy
+        plt.axis('off')  # Wyłączenie osi dla lepszej czytelności
         
         plt.subplot(2, num_samples, num_samples + i + 1)  # Drugi rząd: momenty Hu
-        hu_text = '\n'.join([f'{moment:.2e}' for moment in hu_moments[i]])  # Formatowanie momentów Hu
-        plt.text(0.5, 0.5, hu_text, ha='center', va='center', wrap=True)
-        plt.axis('off')
+        hu_text = '\n'.join([f'Hu {j+1}: {moment:.2e}' for j, moment in enumerate(hu_moments[i])])  # Formatowanie momentów Hu z numerami
+        plt.text(0.5, 0.5, hu_text, ha='center', va='center', wrap=True)  # Wyświetlenie momentów Hu w centrum
+        plt.axis('off')  # Wyłączenie osi dla lepszej czytelności
         
-    plt.show()
+    plt.suptitle('Przykładowe próbki i ich momenty Hu')  # Dodanie tytułu wykresu
+    plt.tight_layout(rect=[0, 0, 1, 0.95])  # Zarezerwowanie miejsca na tytuł
+    plt.show()  # Wyświetlenie wykresu
 
 if __name__ == '__main__':
+    # Sprawdzenie, czy podano odpowiednią liczbę argumentów wiersza poleceń
     if len(sys.argv) != 2:
         print("Usage: python script.py <data_directory>")
         sys.exit(1)
@@ -43,26 +46,26 @@ if __name__ == '__main__':
     # Wyświetlanie przykładowych obrazów z zestawu treningowego wraz z momentami Hu
     show_sample_images(X_train, y_train, hu_train)
 
-# Opis funkcji:
-# show_sample_images(images, labels, hu_moments, num_samples=10)
+# Dokumentacja:
+# Funkcja show_sample_images(images, labels, hu_moments, num_samples=10):
 
-# Wyświetla próbki obrazów z odpowiadającymi im momentami Hu w formacie logarytmicznym.
-
-#     Parameters:
+#     Opis: Wyświetla próbki obrazów z odpowiadającymi im momentami Hu.
+#     Parametry:
 #         images (ndarray): Tablica z obrazami.
 #         labels (ndarray): Tablica z etykietami klas.
 #         hu_moments (ndarray): Tablica z momentami Hu.
 #         num_samples (int): Liczba próbek do wyświetlenia (domyślnie 10).
+#     Opis działania:
+#         Funkcja ustawia rozmiar figury, aby zmieścić więcej informacji.
+#         W pierwszym rzędzie wyświetlane są obrazy w skali szarości z tytułem zawierającym etykietę klasy.
+#         W drugim rzędzie wyświetlane są odpowiednie momenty Hu w formacie tekstowym, wraz z ich numerami.
+#         Dodany jest tytuł wykresu "Przykładowe próbki i ich momenty Hu".
 
-# Szczegóły implementacji:
+# Sekcja if __name__ == '__main__'::
 
-#     Funkcja plt.figure(figsize=(10, 4)) ustawia wielkość figury, zwiększając jej wysokość, aby zmieścić dwa rzędy podwyświetla.
-#     Pierwszy rząd (pierwsze plt.subplot) wyświetla obrazy w skali szarości wraz z etykietami.
-#     Drugi rząd (drugie plt.subplot) wyświetla znormalizowane momenty Hu w formacie logarytmicznym dla każdego obrazu.
-#     Moment Hu jest sformatowany do notacji wykładniczej z dwoma miejscami po przecinku (f'{moment:.2e}'), co jest odpowiednie do wyświetlania małych wartości.
-#     plt.axis('off') wyłącza osie dla lepszego wyglądu wyświetlania.
-
-# Sposób użycia:
-
-#     Wczytaj przetworzone dane (obrazy, etykiety, momenty Hu) z plików .npy.
-#     Wywołaj funkcję show_sample_images, aby wyświetlić próbki obrazów z zestawu treningowego wraz z odpowiadającymi im momentami Hu.
+#     Opis: Sekcja główna skryptu, która jest wykonywana, gdy skrypt jest uruchamiany bezpośrednio.
+#     Działanie:
+#         Sprawdza, czy podano odpowiednią liczbę argumentów wiersza poleceń.
+#         Pobiera ścieżkę do katalogu z danymi z argumentu wiersza poleceń.
+#         Wczytuje przetworzone dane (obrazy, etykiety, momenty Hu) z plików .npy.
+#         Wywołuje funkcję show_sample_images, aby wyświetlić próbki obrazów z zestawu treningowego wraz z odpowiadającymi im momentami Hu.
